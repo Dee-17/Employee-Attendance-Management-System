00371d0ebf3a24f645b52af7f791237953676e90
Guirald Escober <guiraldmaloneespaldon.escober@bicol-u.edu.ph>
2023-12-01 15:29:33 +0800

    Update admin-login.php

---
+++ admin-login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '/xampp/htdocs/Employee-Attendance-Management/php/head.php'?>
+    <title>Admin Login</title>
+</head>
+<body>
+  
+</body>
+</html>
