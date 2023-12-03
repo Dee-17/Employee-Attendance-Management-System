368a27a7b574fe33d594cc2abacd23dab46ea2f6
Guirald Escober <guiraldmaloneespaldon.escober@bicol-u.edu.ph>
2023-12-03 15:36:40 +0800

    Sample Data Added For Admins

--- sql-create/database-create.php
+++ sql-create/database-create.php
@@ -48,8 +48,50 @@ function createAdminTable($conn, $db_name) {
             echo "Error creating table: " . $conn->error;
         }
     } else {
-        echo "Table admin already exists.Inserting Data";
-        $sql = "INSERT INTO admin (user_name, password, name) VALUES ('miccch', 'macho123', 'Mitch Gumabao')";
+        echo "Table admin already exists. Proceeding to insert data...";
+        $sql = "INSERT INTO admin (user_name, password, name) VALUES ('miccch', 'macho123', 'Mitch Gumabao'),
+                                                                     ('archiee','mekusmekus','Archie Arjon'),
+                                                                     ('lowsie','mahinahina','Lucy Incan'),
+                                                                     ('user1','user123','User')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Data inserted successfully";
+        } else {
+            echo "Error inserting data: " . $conn->error;
+        }
+    }
+
+}
+
+// Function to create table "user"
+function createUserTable($conn, $db_name) {
+    // Check if table exists
+    $sql = "SHOW TABLES FROM " . $db_name . " LIKE 'user'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 0) {
+        // Create table
+        $sql = "CREATE TABLE user (
+            user_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+            user_name VARCHAR(50) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            name VARCHAR(100) NOT NULL
+        )";
+        if ($conn->query($sql) === TRUE) {
+            echo "Table user created successfully";
+        } else {
+            echo "Error creating table: " . $conn->error;
+        }
+    } else {
+        echo "Table user already exists. Proceeding to insert data...";
+        $sql = "INSERT INTO user (user_name, password, name) VALUES ('johndoe', 'john123', 'John Doe'),
+                                                                     ('jane123','jane123','Jane Doe'),
+                                                                     ('samuel24','samuel123','Samuel Smith'),
+                                                                     ('user2','user123','User')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Data inserted successfully";
+        } else {
+            echo "Error inserting data: " . $conn->error;
+        }
     }
 
 }
@@ -57,8 +99,11 @@ function createAdminTable($conn, $db_name) {
 // Connect to the specific database
 $conn->select_db($db_name);
 
-// Create the table
+// Create the admin table
 createAdminTable($conn, $db_name);
+
+// Create the user table
+createUserTable($conn, $db_name);
 
 $conn->close();
 ?>
