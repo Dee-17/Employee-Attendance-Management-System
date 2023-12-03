7a1463322945d9914182a0096efa390b92c64ce9
Guirald Escober <guiraldmaloneespaldon.escober@bicol-u.edu.ph>
2023-12-03 16:34:42 +0800

    admin-login header change
    change file document from html to php to execute php commands

--- admin-login.php
+++ admin-login.php
@@ -1,22 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php
-        include '/xampp/htdocs/Employee-Attendance-Management/php/database-check.php'; 
-        include '/xampp/htdocs/Employee-Attendance-Management/php/head.php';
+    <?php 
+        include '/xampp/htdocs/Employee-Attendance-Management/php/head.php'
     ?>
-    <title>Admin Login</title>
+    <title>Admin Log-in</title>
 </head>
 <body>
-    <form action="">
-        <br><br>
-        <label for="user_name">user_name</label>
-        <input type="text" name="user_name">
-        <br><br>
-        <label for="password">password</label>
-        <input type="password" name="password">
-        <br><br>
-        <input type="submit">
-    </form>
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="3" method="post">
+                <h1>Create Account</h1>
+                <div class="infield">
+                    <input type="text" placeholder="Name" />
+                    <label></label>
+                </div>
+                <div class="infield">
+                    <input type="email" placeholder="Email" name="email"/>
+                    <label></label>
+                </div>
+                <div class="infield">
+                    <input type="password" placeholder="Password" />
+                    <label></label>
+                </div>
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Sign in</h1>
+                <div class="infield">
+                    <input type="email" placeholder="Email" name="email"/>
+                    <label></label>
+                </div>
+                <div class="infield">
+                    <input type="password" placeholder="Password" />
+                    <label></label>
+                </div>
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container" id="overlayCon">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button>Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+            <button id="overlayBtn"></button>
+        </div>
+    </div>
+
+    
+    <script>
+        const container = document.getElementById('container');
+        const overlayCon = document.getElementById('overlayCon');
+        const overlayBtn = document.getElementById('overlayBtn');
+
+        overlayBtn.addEventListener('click', () => {
+            container.classList.toggle('right-panel-active');
+            overlayCon.classList.toggle('overlay-active');
+        });
+        overlayBtn.classList.remove('btnScaled');
+        window.requestAnimationFrame( () => {
+            overlayBtn.classList.add('btnScaled');
+        });
+
+
+
+    </script>
+
 </body>
 </html>
