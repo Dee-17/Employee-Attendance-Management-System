e0734ad097e17555f26e09a1d688584ece812188
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-06 00:20:11 +0800

    Updated UI layout

--- daily-report.php
+++ daily-report.php
@@ -20,23 +20,98 @@
 
             <!-- Links / buttons -->
             <nav class="nav flex-column nav-pills mt-4 p-2 gap-2">
-                <a class="nav_button nav-link" href="login-report.html">Log in Report</a>
-                <a class="nav_button nav-link" href="employee-maintenance.html">Employee Maintenance</a>
-                <a class="nav_button nav-link" href="daily-report.html">Daily Report</a>
-                <a class="nav_button nav-link" href="monthly-report.html">Monthly Report</a>
-                <span class="log_out_button mb-3"><a class="nav_button nav-link" href="employee-login.html">Log out</a></span>
+                <a class="nav_button nav-link" href="login-report.php">Log in Report</a>
+                <a class="nav_button nav-link" href="employee-maintenance.php">Employee Maintenance</a>
+                <a class="nav_button nav-link" href="daily-report.php">Daily Report</a>
+                <a class="nav_button nav-link" href="monthly-report.php">Monthly Report</a>
+                <span class="log_out_button mb-3"><a class="nav_button nav-link" href="employee-login.php">Log out</a></span>
             </nav>
         </div>
 
         <!-- Main contents -->
         <div class="right_panel container p-5">    
             <p class="header_title"><span class="bold_title">Daily Log in</span> Report</p>
-            <div class="container mt-4 row gap-3">
-                <div class="col col-8 p-0">
-
+            <!-- First row -->
+            <div class="row container-fluid mt-4 gap-3 d-flex justify-content-between">
+                <div class="col col-7 p-0">
+                    <!-- Display the date chosen by user -->
+                    <div class="row m-0 p-0 gap-3">
+                        <div class="date_container card px-4 py-2 col col-8">
+                            <p class="date_subtitle">Viewing log in reports during</p>
+                            <p class="date_title">DATE HERE</p>
+                        </div>
+                        <div class="card col col-2 p-0">
+                            calendar
+                        </div>
+                    </div>
+                </div>
+                <!-- Display real-time clock -->
+                <div class="card col col-4 p-0 m-0">
+                    time nalang
                 </div>
             </div>
+
+            <!-- Reports -->
+            <div class="white_container row mt-3 p-4 mx-0 text-center justify-content-evenly">
+                <table class="employee_table table">
+                    <thead class="table_header">
+                        <tr>
+                            <th scope="col">Emp ID</th>
+                            <th scope="col">Full Name</th>
+                            <th scope="col">Contract</th>
+                            <th scope="col">Shift</th>
+                            <th scope="col">AM IN</th>
+                            <th scope="col">AM OUT</th>
+                            <th scope="col">PM IN</th>
+                            <th scope="col">PM OUT</th>
+                            <th scope="col">Late</th>
+                            <th scope="col">Undertime</th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="">
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark Otto</td>
+                            <td>Full-time</td>
+                            <td>Whole Day</td>
+                            <td>8:00</td>
+                            <td>12:01</td>
+                            <td>1:00</td>
+                            <td>5:01</td>
+                            <td>0</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Mark Otto</td>
+                            <td>Part-time</td>
+                            <td>Afternoon</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>1:00</td>
+                            <td>4:51</td>
+                            <td>0</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Mark Otto</td>
+                            <td>Part-time</td>
+                            <td>Morning</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>9:00</td>
+                            <td>12:51</td>
+                            <td>0</td>
+                            <td>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
+
     </div>
 </body>
 </html>
