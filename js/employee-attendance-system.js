e6fa94bfae470239b94190cc600de8e81223071f
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-05 14:39:01 +0800

    Added functionality for date and time

---
+++ js/employee-attendance-system.js
@@ -0,0 +1,28 @@
+function date_time() {         
+    var month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var day_name = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var today = new Date();
+
+    // sets date for the day
+    document.getElementById('day_today').innerHTML = (day_name[today.getDay()]);
+    document.getElementById('full_date').innerHTML = (month_name[today.getMonth()] + ' ' + today.getDate() + ', ' + today.getFullYear()).toUpperCase();
+
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var second = today.getSeconds();
+    var day = hour<12 ? "AM": "PM";
+
+    hour = hour<10 ? '0'+hour: hour;
+    minute = minute<10 ? '0'+minute: minute;
+    second = second<10 ? '0'+second: second;
+
+    // convert into 12hr format
+    var new_hour = hour>12 ? hour-12: hour;
+
+    document.getElementById('hour').innerHTML = new_hour;
+    document.getElementById('minute').innerHTML = minute;
+    document.getElementById('second').innerHTML = second;
+    document.getElementById('am_pm').innerHTML = day;
+
+} var inter = setInterval(date_time,1000)
+
