99983d03804411f843b15707c4d656896402f99e
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-07 22:39:57 +0800

    Added comments

--- js/date_time.js
+++ js/date_time.js
@@ -6,23 +6,21 @@ function date_time() {
         // sets date for the day
         document.getElementById('day_today').innerHTML = (day_name[today.getDay()]);
         document.getElementById('full_date').innerHTML = (month_name[today.getMonth()] + ' ' + today.getDate() + ', ' + today.getFullYear()).toUpperCase();
-    
     } catch (error) {
         // wala lang
     }
-    
+    // get the current time
     var hour = today.getHours();
     var minute = today.getMinutes();
     var second = today.getSeconds();
     var day = hour<12 ? "AM": "PM";
-
+    // format
     hour = hour<10? '0'+hour: hour;
     minute = minute<10? '0'+minute: minute;
     second = second<10? '0'+second: second;
-
     // convert into 12hr format
     var new_hour = hour>12 ? hour-12: hour;
-
+    // display current time
     document.getElementById('hour').innerHTML = new_hour;
     document.getElementById('minute').innerHTML = minute;
     document.getElementById('second').innerHTML = second;
