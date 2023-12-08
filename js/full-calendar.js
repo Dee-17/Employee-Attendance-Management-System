5bb883e265bfcb2a0d4682e9a9eb8e3e7ce79f5a
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-08 11:51:47 +0800

    Modified comments

--- js/full-calendar.js
+++ js/full-calendar.js
@@ -8,12 +8,12 @@ document.getElementById('search_button').addEventListener('click', function() {
         // format date
         var date_parts = selected_date.split("-");
         var date = new Date(date_parts[0], date_parts[1]-1, date_parts[2]);        
-        var formatted_date = date.toLocaleDateString('en-US', {day: '2-digit', month: '2-digit', year: 'numeric'}); // Format the date as desired
+        var formatted_date = date.toLocaleDateString('en-US', {day: '2-digit', month: '2-digit', year: 'numeric'});
         // display date on screen
         var month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         document.getElementById('selected_date').innerHTML = (month_name[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()).toUpperCase();
-        // close modal
     }
+    // close modal
     $('#calendar_modal').modal('hide'); 
     });
     // changes the container shown on screen
