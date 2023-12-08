a5902d77f86b8b3d2452babb3fa6ab79248421a1
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-08 11:42:34 +0800

    Renamed stylesheet link

--- login-report.php
+++ login-report.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in Reports</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/nav_bar.css">
+    <link rel="stylesheet" href="css/nav-bar.css">
     <link rel="stylesheet" href="css/login-report.css">
     <script src="js/nav-bar.js" defer></script>
     <script src="js/date-time.js" defer></script>
