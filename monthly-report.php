77092406101030e93f66245d62194f5b53c6ee8f
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-08 12:01:04 +0800

    Renamed stylesheet link

--- monthly-report.php
+++ monthly-report.php
@@ -6,7 +6,7 @@
     <title>Monthly Reports</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">    
-    <link rel="stylesheet" href="css/monthly-report.css">
+    <link rel="stylesheet" href="css/month-report.css">
     <link rel="stylesheet" href="css/nav-bar.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
