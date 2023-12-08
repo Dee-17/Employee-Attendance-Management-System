8dece9b1c1a58d6c6d2c289f40648844b85c6ceb
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-08 11:28:44 +0800

    Updated syntax

--- js/full-calendar.js
+++ js/full-calendar.js
@@ -1,14 +1,19 @@
 // output the selected date
 document.getElementById('search_button').addEventListener('click', function() {
     var selected_date = document.getElementById('picked_date').value;
-    // format date
-    var date_parts = selected_date.split("-");
-    var date = new Date(date_parts[0], date_parts[1]-1, date_parts[2]);        
-    var formatted_date = date.toLocaleDateString('en-US', {day: '2-digit', month: '2-digit', year: 'numeric'}); // Format the date as desired
-    // display date on screen
-    var month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    document.getElementById('selected_date').innerHTML = (month_name[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()).toUpperCase();
-    // close modal
+    // display an alert if there's no date entered
+    if(selected_date == "") {
+        document.getElementById('selected_date').innerHTML = "NO DATE SELECTED";
+    } else {
+        // format date
+        var date_parts = selected_date.split("-");
+        var date = new Date(date_parts[0], date_parts[1]-1, date_parts[2]);        
+        var formatted_date = date.toLocaleDateString('en-US', {day: '2-digit', month: '2-digit', year: 'numeric'}); // Format the date as desired
+        // display date on screen
+        var month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        document.getElementById('selected_date').innerHTML = (month_name[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()).toUpperCase();
+        // close modal
+    }
     $('#calendar_modal').modal('hide'); 
     });
     // changes the container shown on screen
