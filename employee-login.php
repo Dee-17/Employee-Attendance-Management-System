607ba075ec52f520d2184af2ec888e191d2b5693
Guirald Escober <guiraldmaloneespaldon.escober@bicol-u.edu.ph>
2023-12-09 20:37:21 +0800

    start employee login

--- employee-login.php
+++ employee-login.php
@@ -1,3 +1,31 @@
+<?php
+    include "connection.php";
+
+    if (isset($_POST['signin'])) {
+        $id = $_POST['id-number'];
+        $password = $_POST['password'];
+
+        if (empty($id) || empty($password)) {
+            echo "Warning: Please fill all the fields";
+        } else {
+            $query = "SELECT * FROM employee WHERE id = '$id'";
+            $result = mysqli_query($conn, $query);
+
+            if (mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+
+                if ($password == $row['password']) {
+                    header("Location: employee-dashboard.php");
+                } else {
+                    echo "Warning: Incorrect password";
+                }
+            } else {
+                echo "Warning: No user found with this ID";
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +34,7 @@
     <title>Landing Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="css/employee-login.css">
-  
+ 
 </head>
 <body>
     <div class="container" id="container">
@@ -22,20 +50,20 @@
         <div class="content-right">
             <div class="form-container sign-in-container">
                 <a href="landing-page.php"><button id="exit-button"><img id="exit-button-img" src="images/quit.png"></button></a>
-                <form action="#">
+                <form action="" method="post">
                     <h1>Employee Log In</h1>
                     <div class="infield">
                         <input type="text" placeholder="ID Number" name="id-number"/>
                         <label></label>
                     </div>
                     <div class="infield">
-                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" name="password"/>
                         <label></label>
                     </div>
-                    <button>Sign In</button>
+                    <button type="submit" name="signin">Sign In</button>
+                </form>
             </div>
         </div>
     </div>
-
 </body>
 </html>
