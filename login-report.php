68b4bcffe3bb1b36256bb4735e6109dbdfa6c49f
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-12 11:39:07 +0800

    Added legend and modified table

--- login-report.php
+++ login-report.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in Reports</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="css/nav-bar.css">
     <link rel="stylesheet" href="css/login-report.css">
     <script src="js/nav-bar.js" defer></script>
@@ -25,12 +26,12 @@
         <!-- Main contents -->
         <div class="right_panel container p-5">
             <!-- Name must be according to id inputted by admin -->
-            <p class="title_2">Welcome <span class="admin_name" id="admin_name">ADMIN123</span>!</p>
+            <p class="title_1">Welcome <span class="admin_name" id="admin_name">ADMIN123</span>!</p>
             <div class="container mt-4 row gap-3">
                 <div class="col col-12 p-0">
                     <div class="row m-0 gap-3">
                         <!-- Date today -->
-                        <div class="date_container card px-4 py-2 col col-4">
+                        <div class="date_container card px-4 py-2 col col-4 justify-content-center">
                             <p class="date_subtitle m-0 p-0">Today is <span class="day_title" id="day_today"></span></p>
                             <span class="date_title"><p class="mb-0 p-0" id="full_date"></p></span>
                         </div>
@@ -45,6 +46,12 @@
                                 <span id="am_pm"></span>
                             </div>
                         </div>
+                        <!-- Table legend -->
+                        <div class="white_container col col-2 m-0 py-3 px-4">
+                            <p class="title_2 text-center">Table legend</p>
+                            <div class="legend_red"><i class="bi bi-square-fill"></i><span class="mx-1">Late</span></div>
+                            <div class="legend_blue"><i class="bi bi-square-fill"></i><span class="mx-1">Undertime</span></div>
+                        </div>
                     </div>
 
                     <div class="white_container row mt-3 p-4 mx-0 text-center justify-content-evenly">
@@ -57,6 +64,7 @@
                                 <th scope="col">AM Out</th>
                                 <th scope="col">PM In</th>
                                 <th scope="col">PM Out</th>
+                                <th scope="col">Overtime</th>
                                 <th scope="col">Night Differential</th>
                               </tr>
                             </thead>
@@ -76,14 +84,15 @@
                                         echo "<td>" . $row["pm_in"] . "</td>";
                                         echo "<td>" . $row["pm_out"] . "</td>";
                                         echo "<td>" . $row["night_differential"] . "</td>";
+                                        echo "<td>" . $row["night_differential"] . "</td>";
                                         echo "</tr>";
                                     }
                                     echo "</tbody></table>";
                                 }
                                 else {
-                                    echo "</tbody></table>No Employees Found";
+                                    echo "</tbody></table>";
+                                    echo "No Employees Found";
                                 }
-
                                 $conn->close();
                             ?>
                 </div>
