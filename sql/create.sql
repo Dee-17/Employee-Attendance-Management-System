4a8440ca4628e2deb57dc2ffddab5bb1d31a25b3
Daniela Cantillo <danielamcantillo@gmail.com>
2023-12-13 13:03:24 +0800

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -9,18 +9,24 @@ CREATE TABLE admin(
     full_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE employee(
-    emp_id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(25) NOT NULL,
-    password VARCHAR(30) NOT NULL,
-    full_name VARCHAR(30) NOT NULL,
-    contract VARCHAR(50) NOT NULL,
-    shift VARCHAR(25)NOT NULL
-    );
+CREATE TABLE employee (
+  emp_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  password varchar(30) NOT NULL,
+  full_name varchar(30) NOT NULL,
+  first_name varchar(50) DEFAULT NULL,
+  middle_name varchar(50) DEFAULT NULL,
+  last_name varchar(50) DEFAULT NULL,
+  address varchar(100) DEFAULT NULL,
+  zip varchar(4) DEFAULT NULL,
+  contact_number varchar(11) DEFAULT NULL,
+  email_address varchar(50) DEFAULT NULL,
+  contract varchar(50) NOT NULL,
+  shift varchar(25) NOT NULL
+);
 
 CREATE TABLE atlog(
-    atlog_id INTEGER NOT NULL auto_increment,
-    emp_id INTEGER NOT NULL,
+    atlog_id INT NOT NULL auto_increment,
+    emp_id INT NOT NULL,
     atlog_DATE DATE,
     am_in TIME NULL,
     am_out TIME NULL,
@@ -36,7 +42,6 @@ CREATE TABLE atlog(
     FOREIGN KEY (emp_id) REFERENCES employee(emp_id)
 );
 
-ALTER TABLE `atlog` ADD CONSTRAINT `fk_atlog_employee` FOREIGN KEY (`emp_id`) REFERENCES `employee`(`emp_id`);
 
 INSERT INTO admin (username, password, full_name) 
 VALUES  ('admin1', 'admin123', 'Michael Lariosa'),
@@ -44,10 +49,10 @@ VALUES  ('admin1', 'admin123', 'Michael Lariosa'),
         ('admin3', 'aragorn', 'Aram Muncal')
         ;
 
-INSERT INTO employee (username, password, full_name) 
-VALUES  ('emp1', 'emp123', 'Alex Scenariosa'),
-        ('emp2', 'powwwy', 'Powwwy March'),
-        ('emp3', 'agedwine', 'Charles Baclao')
+INSERT INTO employee (password, full_name) 
+VALUES  ('emp123', 'Alex Scenariosa'),
+        ('powwwy', 'Powwwy March'),
+        ('agedwine', 'Charles Baclao')
         ;
 
 INSERT INTO atlog (emp_id, atlog_DATE, am_in, am_out, pm_in, pm_out) 
