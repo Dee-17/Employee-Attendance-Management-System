120b872a069948026e563e20dd5543bb6a267979
Daniela Cantillo <danielamcantillo@gmail.com>
2023-12-13 20:15:13 +0800

    Fixed login report

--- login-report.php
+++ login-report.php
@@ -1,6 +1,6 @@
 <?php
     include "update.php";
-    $sql = "SELECT atlog.emp_id, employee.first_name, employee.last_name, employee.middle_name, employee.shift,employee.contract, atlog.am_in, atlog.am_out, atlog.pm_in, atlog.pm_out, atlog.am_late, atlog.pm_late, atlog.am_underTIME, atlog.pm_underTIME, atlog.night_differential
+    $sql = "SELECT atlog.emp_id, employee.full_name, atlog.am_in, atlog.am_out, atlog.pm_in, atlog.pm_out, atlog.am_late, atlog.pm_late, atlog.am_underTIME, atlog.pm_underTIME, atlog.night_differential
     FROM atlog
     JOIN employee ON atlog.emp_id = employee.emp_id
     WHERE atlog.atlog_DATE = CURDATE();";
@@ -76,7 +76,7 @@
                             while ($row = mysqli_fetch_assoc($result)) {
                                 echo "<tr>";
                                 echo "<td>" . $row["emp_id"] . "</td>";
-                                echo "<td>" . $row["first_name"]  .  $row['middle_name'].$row['last_name']. "</td>";
+                                echo "<td>" . $row["full_name"]  . "</td>";
                                 echo "<td>" . $row["am_in"] . "</td>";
                                 echo "<td>" . $row["am_out"] . "</td>";
                                 echo "<td>" . $row["pm_in"] . "</td>";
