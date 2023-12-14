51663ef528434082eeba26cf16f6f062a3ac4ee9
Joseph Riosa <125950765+AlexHereToCode@users.noreply.github.com>
2023-12-14 14:38:32 +0800

    Update login-report.php
    added display of admin name

--- login-report.php
+++ login-report.php
@@ -1,103 +1,145 @@
 <?php
-    include "connection.php";
     session_start();
+    include "update.php";
     
-    if (isset($_POST['form_id'])) {
-        // prepare and bind
-        $stmt = $conn->prepare("SELECT admin_id, username, password FROM admin WHERE username = ? AND password = ?");
-        $stmt->bind_param("ss", $username, $password);
+    // Check if employee ID is set in the session
+    if (isset($_SESSION['emp_id'])) {
+        $empId = $_SESSION['emp_id']; 
 
-        // set parameters and execute
-        $username = $_POST['sn-username'];
-        $password = $_POST['sn-password'];
-        $stmt->execute();
+        $sql = "SELECT atlog.emp_id, employee.first_name, employee.middle_name, employee.last_name, employee.shift, atlog.am_in, atlog.am_out, atlog.pm_in, atlog.pm_out, atlog.am_late, atlog.pm_late, atlog.am_underTIME, atlog.pm_underTIME, atlog.overtime, atlog.night_differential
+                FROM atlog 
+                JOIN employee ON atlog.emp_id = employee.emp_id
+                WHERE atlog.emp_id = $empId AND atlog.atlog_DATE = CURDATE()";
 
-        // get the result
-        $result = $stmt->get_result();
+        $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
-            // Fetch the admin ID and store it in the session
             $row = $result->fetch_assoc();
-            $_SESSION['admin_id'] = $row['admin_id'];
-
-            // redirect
-            header("Location: login-report.php");
+            $employeeFullName = $row['first_name'] . ' ' . $row['middle_name'] . ' ' . $row['last_name'];
         } else {
-            if (empty($username) || empty($password)) {
-                $sn_missing = true; 
-            } else {
-                // output a warning saying incorrect details
-                $sn_error = true;
-            }
+            echo '<p>No previous entry found</p>';
         }
+    } else {
+        echo '<p>Employee ID not set in the session</p>';
+    }
 
-        $stmt->close();
-        $conn->close();
-    } 
+    if (isset($_SESSION['admin_id'])) {
+        $adminId = $_SESSION['admin_id']; 
+
+        $adminUsernameQuery = "SELECT username FROM admin WHERE admin_id = $adminId";
+        
+        $adminResult = $conn->query($adminUsernameQuery);
+
+        if ($adminResult->num_rows > 0) {
+            $adminRow = $adminResult->fetch_assoc();
+            $adminUsername = $adminRow['username'];
+        } else {
+            echo '<p>No admin entry found</p>';
+        
+            $adminUsername = ''; 
+        }
+    } else {
+        echo '<p>Admin ID not set in the session</p>';
+        $adminUsername = ''; 
+    }
+
+    $conn->close();
 ?>
- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Log in</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <title>Log in Reports</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="css/admin-emp-login.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/nav-bar.css">
+    <link rel="stylesheet" href="css/login-report.css">
+    <script src="js/nav-bar.js" defer></script>
+    <script src="js/date-time.js" defer></script>
 </head>
-<body>
-    <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
-        <div class="login_container card rounded-4 shadow-lg m-0 p-0 overflow-hidden">
-            <!-- Back button -->
-            <div class="top_panel p-3 d-flex justify-content-center">
-                <div class="row container-fluid justify-content-end">
-                    <a href="landing-page.php"><i class="bi bi-arrow-left" style="color: white;"></i></a>
+<body class="container-fluid">
+    <div class="container-fluid row gap-0">
+        <?php 
+            include('nav-bar.php');
+        ?> 
+        <!-- Main contents -->
+        <div class="right_panel container p-5">
+            <!-- Name must be according to id inputted by admin -->
+            <p class="header_title">Welcome <span class="admin_name" id="admin_name"><?php echo htmlspecialchars($adminUsername); ?></span>!</p>
+            <div class="row container-fluid m-0 gap-3">
+                <!-- Date today -->
+                <div class="date_container card px-4 py-2 col col-4 justify-content-center">
+                    <p class="date_subtitle m-0 p-0">Today is <span class="day_title" id="day_today"></span></p>
+                    <span class="date_title"><p class="mb-0 p-0" id="full_date"></p></span>
+                </div>
+                <!-- Real-time clock -->
+                <div class="clock_container grey_container col col-4 m-0 p-0 ms-auto">
+                    <div class="clock_elements">
+                        <span id="hour"></span>
+                        <span id="point">:</span>
+                        <span id="minute"></span>
+                        <span id="point">:</span>
+                        <span id="second"></span>
+                        <span id="am_pm"></span>
+                    </div>
+                </div>
+                <!-- Table legend -->
+                <div class="white_container col col-2 m-0 py-3 px-4">
+                    <p class="legend_title text-center">Table legend</p>
+                    <div class="legend_red"><i class="bi bi-square-fill"></i><span class="mx-1">Late</span></div>
+                    <div class="legend_blue"><i class="bi bi-square-fill"></i><span class="mx-1">Undertime</span></div>
                 </div>
             </div>
-            <!-- Admin log in -->
-            <div class="row bottom_panel mx-5 my-4 p-0">
-                <form class="m-0 p-0" action="" method="post">
-                    <!-- Displays an error -->
-                    <?php if (isset($sn_error)): ?>
-                        <!-- If the credentials are incorrect -->
-                        <div class="alert alert-danger py-3 mb-3 d-flex justify-content-between" role="alert">
-                            <div class="alert_mes">Incorrect username or password!</div>
-                            <button class='btn-close' data-bs-dismiss='alert' aria-label='Close' type='button'></button>
-                        </div>
-                    <?php endif; ?>
 
-                    <?php if (isset($sn_missing)): ?>
-                        <!-- If there are missing fields -->
-                        <div class="alert alert-warning py-3 mb-3 d-flex justify-content-between" role="alert">
-                            <div class="alert_mes">Please fill out all fields!</div>
-                            <button class='btn-close' data-bs-dismiss='alert' aria-label='Close' type='button'></button>
-                        </div>
-                    <?php endif; ?>
-                    
-                    <!-- ADMIN LOGIN FORM -->
-                    <p class="text-center">Admin Log In</p>
-                    <div class="mt-3">
-                        <label for="sn-username">Username</label>
-                        <input class="form-control" type="text" placeholder="Enter admin username" id="sn-username" name="sn-username"/>
-                    </div>
-                    <div class="mt-2 mb-3">
-                        <label for="sn-password">Password</label>
-                        <input class="form-control" type="password" placeholder="Enter password" id="sn-password" name="sn-password" />
-                    </div>
-                    <input type="hidden" name="form_id" value="1">
-                    <div class="login_button d-flex justify-content-center align-items-center">
-                        <button class="my-3 w-50 text-center btn btn-primary" type="submit">Log In</button>
-                    </div>
-                    <!-- Option to create an account -->
-                    <div class="mt-3 create_account text-center">
-                        <p>Need an account? <span class="bold_title"><a class="nav_link" href="admin-sign-up.php">Create an account</a></span></p>
-                    </div>
-                </form>
-            </div>
+            <div class="white_container row mt-3 p-4 mx-0 text-center justify-content-evenly">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Emp ID</th>
+                            <th scope="col">Full Name</th>
+                            <th scope="col">Shift</th>
+                            <th scope="col">AM In</th>
+                            <th scope="col">AM Out</th>
+                            <th scope="col">PM In</th>
+                            <th scope="col">PM Out</th>
+                            <th scope="col">Overtime</th>
+                            <th scope="col">Night Differential</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table_body">
+                    <?php
+                        include "connection.php";
+
+                        $result = $conn->query($sql);
+                        
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo "<tr>";
+                                echo "<td>" . $row["emp_id"] . "</td>";
+                                echo "<td>" . $row["first_name"] . " " . $row["middle_name"] . " " . $row["last_name"] . "</td>";
+                                echo "<td>" . $row["shift"] . "</td>";
+                                echo "<td>" . $row["am_in"] . "</td>";
+                                echo "<td>" . $row["am_out"] . "</td>";
+                                echo "<td>" . $row["pm_in"] . "</td>";
+                                echo "<td>" . $row["pm_out"] . "</td>";
+                                echo "<td>" . $row["overtime"] . "</td>";
+                                echo "<td>" . $row["night_differential"] . "</td>";
+                                echo "</tr>";
+                            }
+                            echo "</tbody></table>";
+                        }
+                        else {
+                            echo "</tbody></table>";
+                            echo "
+                                <div class='alert alert-danger m-0 p-3' role='alert'>
+                                No Records Found
+                                </div>
+                            ";
+                        }
+                        $conn->close();
+                    ?>
         </div>
     </div>
 </body>
-</html> 
+</html>
