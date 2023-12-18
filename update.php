cffe5ec1cf14c661c89d117a2a372dab3149a184
Guirald Escober <guiraldmaloneespaldon.escober@bicol-u.edu.ph>
2023-12-18 17:48:06 +0800

    Updated overtime attempt

--- update.php
+++ update.php
@@ -7,19 +7,19 @@ $sql = "UPDATE atlog
             pm_late = IF(TIMEDIFF(pm_in, '16:00:00') > '00:00:00', 'YES', 'NO'),
             pm_underTIME = IF(TIMEDIFF('17:00:00', pm_out) > '00:00:00', 'YES', 'NO'),
             work_hour = ADDTIME(IFNULL(TIMEDIFF(pm_out, pm_in), '00:00:00'), IFNULL(TIMEDIFF(am_out, am_in), '00:00:00')),
-            overtime = overtime = 
-                        ADDTIME(
-                            IF(
-                                IFNULL(TIMEDIFF(am_out, am_in), '00:00:00') > '02:00:00',
-                                TIMEDIFF(am_out, am_in),
-                                '00:00:00'
-                            ),
-                            IF(
-                                IFNULL(TIMEDIFF(pm_out, pm_in), '00:00:00') > '02:00:00',
-                                TIMEDIFF(pm_out, pm_in),
-                                '00:00:00'
-                            )
-                        )";
+            overtime = 
+            ADDTIME(
+                IF(
+                    IFNULL(TIMEDIFF(am_out, am_in), '00:00:00') > '04:00:00',
+                    TIMEDIFF(am_out, am_in) - '02:00:00',
+                    '00:00:00'
+                ),
+                IF(
+                    IFNULL(TIMEDIFF(pm_out, pm_in), '00:00:00') > '04:00:00',
+                    TIMEDIFF(pm_out, pm_in) - '02:00:00',
+                    '00:00:00'
+                )
+            )";
 
 $conn->query($sql);
 ?>
