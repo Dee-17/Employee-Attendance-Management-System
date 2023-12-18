4c53d73fa26dd61017f891d6cd0500e43cc4f242
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2023-12-18 16:22:39 +0800

    Added employee information

--- employee-attendance-system.php
+++ employee-attendance-system.php
@@ -67,48 +67,47 @@
     </head>
     <body class="container-fluid">
     <div class="container-fluid row gap-0">
-            <!-- Navigation Bar -->
-            <div class="left_panel sticky-top container text-center pt-3">
-                <img class="mt-3" src="images/company-logo.png" alt="C597 Corporation Logo">
-                <p class="company_name mt-2 mb-0">C597 Corporation</p>
-                <p class="system_name">Employee Attendance System</p>
+        <!-- Navigation Bar -->
+        <div class="left_panel sticky-top container text-center pt-3">
+            <img class="mt-3" src="images/company-logo.png" alt="C597 Corporation Logo">
+            <p class="company_name mt-2 mb-0">C597 Corporation</p>
+            <p class="system_name">Employee Attendance System</p>
 
-                <!-- Links / buttons -->
-                <nav class="nav flex-column nav-pills mt-4 p-2 gap-2">
-                    <a class="nav_button nav-link" href="employee-attendance-system.php">Employee Log in</a>
-                    <span class="log_out_button mb-5"><a class="nav_button nav-link" href="employee-login.php">Log out</a></span>
-                </nav>
-            </div>
-        
-            <div class="right_panel container p-5">
-                <?php
-                
+            <!-- Links / buttons -->
+            <nav class="nav flex-column nav-pills mt-4 p-2 gap-2">
+                <a class="nav_button nav-link" href="employee-attendance-system.php">Employee Log in</a>
+                <span class="log_out_button mb-5"><a class="nav_button nav-link" href="employee-login.php">Log out</a></span>
+            </nav>
+        </div>
+    
+        <div class="right_panel container p-5">
+            <?php
                 if (isset($row['first_name'], $row['middle_name'], $row['last_name'])) {
                     $employeeFullName = $row['first_name'] . ' ' . $row['middle_name'] . ' ' . $row['last_name'];
                 }
-                ?>
-                <p class="header_title">Welcome <span class="employee_name" id="employee_name"><?php echo $employeeFullName; ?></span>!</p>
-                <div class="container mt-4 row gap-3">
-                    <div class="col col-8 p-0">
-                        <div class="row m-0 gap-3">
-                            <div class="date_container card px-4 py-2 col col-5">
-                                <p class="date_subtitle m-0 p-0">Today is <span class="day_title" id="day_today"></span></p>
-                                <!-- Date today -->
-                                <span class="date_title"><p class="mb-0 p-0" id="full_date"></p></span>
-                            </div>                       
-                            <!-- Clock based on local time -->
-                            <div class="clock_container grey_container col col-7-sm m-0 p-0">
-                                <div class="clock_elements">
-                                    <span id="hour"></span>
-                                    <span id="point">:</span>
-                                    <span id="minute"></span>
-                                    <span id="point">:</span>
-                                    <span id="second"></span>
-                                    <span id="am_pm"></span>
-                                </div>
+            ?>
+            <p class="header_title">Welcome <span class="employee_name" id="employee_name"><?php echo $employeeFullName; ?></span>!</p>
+            <div class="container-fluid mt-4 row gap-3">
+                <div class="col col-8 p-0">
+                    <div class="row m-0 gap-3">
+                        <div class="date_container card px-4 py-2 col col-6">
+                            <p class="date_subtitle m-0 p-0">Today is <span class="day_title" id="day_today"></span></p>
+                            <!-- Date today -->
+                            <span class="date_title"><p class="mb-0 p-0" id="full_date"></p></span>
+                        </div>                       
+                        <!-- Clock based on local time -->
+                        <div class="clock_container grey_container col col-5 ms-auto p-0">
+                            <div class="clock_elements">
+                                <span id="hour"></span>
+                                <span id="point">:</span>
+                                <span id="minute"></span>
+                                <span id="point">:</span>
+                                <span id="second"></span>
+                                <span id="am_pm"></span>
                             </div>
                         </div>
-                        <div class="grey_container row mt-3 p-4 mx-0 text-center justify-content-evenly">
+                    </div>
+                    <div class="grey_container row mt-3 p-4 mx-0 text-center justify-content-evenly">
                         <div class="col col-5 card p-3">
                             <div class="am_title container w-50 mb-3 rounded-2 p-2"><p class="m-0">AM</p></div>
                             <form class="in_out_button row gap-2 m-0 p-0 justify-content-center" action="">
@@ -159,63 +158,79 @@
                                     </script>';
                                     ?>
                             </form>
-                            </div>
+                        </div>
                         <div class="col col-5 card p-3">
                             <div class="pm_title container w-50 mb-3 rounded-2 p-2"><p class="m-0">PM</p></div>
                             <form class="in_out_button row gap-2 m-0 p-0 justify-content-center" action="">
                                 <?php
                                     // Display PM button based on condition
                                     if ($row['pm_in'] === null && ($employeeShift == 'Afternoon Shift' || $employeeShift == 'Both')) {
-                                            echo '<button class="btn in_button col col-auto" onclick="logPM()">PM</button>';
-                                        } else {
-                                            echo '<button class="btn in_button col col-auto" disabled>Checked In</button>';
-                                        }
+                                        echo '<button class="btn in_button col col-auto" onclick="logPM()">PM</button>';
+                                    } else {
+                                        echo '<button class="btn in_button col col-auto" disabled>Checked In</button>';
+                                    }
 
-                                        // Add the JavaScript function to log PM time using AJAX
-                                        echo '<script>
-                                                function logPM() {
-                                                    var xhr = new XMLHttpRequest();
-                                                    xhr.open("GET", "log_pm.php", true);
-                                                    xhr.onreadystatechange = function() {
-                                                        if (xhr.readyState == 4 && xhr.status == 200) {
-                                                            // Update the button status or show a success message if needed
-                                                            console.log(xhr.responseText);
-                                                        }
-                                                    };
-                                                    xhr.send();
+                                    // Add the JavaScript function to log PM time using AJAX
+                                    echo '<script>
+                                            function logPM() {
+                                                var xhr = new XMLHttpRequest();
+                                                xhr.open("GET", "log_pm.php", true);
+                                                xhr.onreadystatechange = function() {
+                                                    if (xhr.readyState == 4 && xhr.status == 200) {
+                                                        // Update the button status or show a success message if needed
+                                                        console.log(xhr.responseText);
+                                                    }
+                                                };
+                                                xhr.send();
+                                            }
+                                        </script>';
+
+
+                                    // Display AM out button based on condition
+                                    if ($row['pm_out'] === null && ($employeeShift == 'Afternoon Shift' || $employeeShift == 'Both')) {
+                                        echo '<button class="btn out_button col col-auto" onclick="logPMOut()">OUT</button>';
+                                    } else {
+                                        echo '<button class="btn out_button col col-auto" disabled>Checked Out</button>';
+                                    }
+
+                                    // Add the JavaScript function to log AM out time using AJAX
+                                    echo '<script>
+                                        function logPMOut() {
+                                            var xhr = new XMLHttpRequest();
+                                            xhr.open("GET", "out_pm.php", true);
+                                            xhr.onreadystatechange = function() {
+                                                if (xhr.readyState == 4 && xhr.status == 200) {
+                                                    // Update the button status or show a success message if needed
+                                                    console.log(xhr.responseText);
                                                 }
-                                            </script>';
-
-
-                                            // Display AM out button based on condition
-                                            if ($row['pm_out'] === null && ($employeeShift == 'Afternoon Shift' || $employeeShift == 'Both')) {
-                                            echo '<button class="btn out_button col col-auto" onclick="logPMOut()">OUT</button>';
-                                        } else {
-                                            echo '<button class="btn out_button col col-auto" disabled>Checked Out</button>';
+                                            };
+                                            xhr.send();
                                         }
-
-                                        // Add the JavaScript function to log AM out time using AJAX
-                                        echo '<script>
-                                                function logPMOut() {
-                                                    var xhr = new XMLHttpRequest();
-                                                    xhr.open("GET", "out_pm.php", true);
-                                                    xhr.onreadystatechange = function() {
-                                                        if (xhr.readyState == 4 && xhr.status == 200) {
-                                                            // Update the button status or show a success message if needed
-                                                            console.log(xhr.responseText);
-                                                        }
-                                                    };
-                                                    xhr.send();
-                                                }
-                                            </script>';
+                                    </script>';
                                 ?>
-                                
-                            </form>                  
-                            <!-- Alert Message -->
-                            <!-- <div class="alert_container card mt-3">
-                            
-                            </div> -->
+                            </form>
                         </div>
+                    </div>
+                </div>
+                
+                <div class="col col-3-sm p-0">
+                    <div class="grey_container p-3">
+                        <div class="text-center emp_title mt-2 mb-3">Employee Information</div>
+                        <!-- display employee information -->
+                        <form action="" class="card m-3 p-3 employee_info d-flex gap-2 justify-content-between">
+                            <div class="">
+                                <label for="emp_id" class="form-label m-0">Emp ID</label>
+                                <input class="form-control" type="text" value="1" name="emp_id" disabled readonly>
+                            </div>
+                            <div class="">
+                                <label for="emp_contract" class="form-label m-0">Contract</label>
+                                <input class="form-control" type="text" value="Part Time" name="emp_contract" disabled readonly>
+                            </div>
+                            <div class="">
+                                <label for="emp_shift" class="form-label m-0">Shift</label>
+                                <input class="form-control" type="text" value="Morning" name="emp_shift" disabled readonly>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
