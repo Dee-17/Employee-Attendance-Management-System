3e503068809ecf3199221093ded7035e7019d412
Daniela Cantillo <danielamcantillo@gmail.com>
2023-12-18 15:29:55 +0800

    Fixed monthly search and 'No Records Found' bug

--- monthly-report.php
+++ monthly-report.php
@@ -27,7 +27,7 @@
                 }); 
             $("#search_button").click(function(){
                 var date_pick = document.getElementById('picked_date').value;
-                global_date_pick =date_pick;
+                global_date_pick = date_pick;
                 $("#table_vals").load("monthly-load.php",{
                     select_date: JSON.stringify(date_pick),
                 })
@@ -40,9 +40,21 @@
                     select_date:global_date_pick
                 })
 
-            //send to monthly-load.php
+                // Fetch the JSON data from 'monthly-search.php'
+                $.post('monthly-search.php', { emp_id: employee_Id }, function(data) {
+                    updateEmployeeInfo(data);
+                }, 'json');
+                
+                // Update the employee information based on the JSON data
+                function updateEmployeeInfo(data) {
+                    $('[name="emp_id"]').val(data.emp_id);
+                    $('[name="emp_name"]').val(data.full_name);
+                    $('[name="emp_contract"]').val(data.contract);
+                    $('[name="emp_shift"]').val(data.shift);
+                }
+
+            });
         });
-    });
     </script>
 </head>
 <body class="container-fluid">
@@ -108,6 +120,31 @@
             </div>
             <!-- Employee search and info -->
             <div class="row container-fluid mt-4 gap-3 d-flex justify-content-between gap-2">
+                <!-- Employee information -->
+                <div class="col col-7-sm p-0">
+                    <div class="row m-0 p-0 gap-3">
+                        <div class="card px-3 py-3 col">
+                            <form action="" class="employee_info d-flex gap-2 justify-content-between" id="employee_info">
+                                <div class="col col-1">
+                                    <label for="emp_id" class="form-label m-0">Emp ID</label>
+                                    <input class="form-control" type="text" value="1" name="emp_id" disabled readonly>
+                                </div>
+                                <div class="col col-6-sm employee_name">
+                                    <label for="emp_name" class="form-label m-0">Full name</label>
+                                    <input class="form-control" type="text" value="MARK OTTO" name="emp_name" disabled readonly>
+                                </div>
+                                <div class="col col-3-sm">
+                                    <label for="emp_contract" class="form-label m-0">Contract</label>
+                                    <input class="form-control" type="text" value="Part Time" name="emp_contract" disabled readonly>
+                                </div>
+                                <div class="col col-2-sm">
+                                    <label for="emp_shift" class="form-label m-0">Shift</label>
+                                    <input class="form-control" type="text" value="Morning" name="emp_shift" disabled readonly>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <!-- Employee search -->
                 <div class="col col-4 p-0">
                     <div class="row m-0 p-0 gap-3">
